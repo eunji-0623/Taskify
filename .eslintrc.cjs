c290dea6912b319b715f3a01c4911ec97dfd31ab
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-30 14:38:57 +0900

    프로젝트 초기 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'airbnb', // Airbnb 스타일 가이드 사용
+        'airbnb/hooks',
+        'airbnb-typescript',
+      ],
+      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        // 추가적인 규칙 설정
+        '@typescript-eslint/comma-dangle': ['error', 'never'],
+      },
+    },
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <>프로젝트 초기설정</>;
+}
+
+export default App;

71ff1f694b70bab0138d4054309c81d014d857e1
foxholic9 <roy0915z@naver.com>
2024-05-31 17:05:59 +0900

    Chore: eslint, prettier 설정 추가
    prettierrc.json 추가.
    1.탭 스페이스 2칸
    2.문자열에 작은 따옴표 사용
    3.객체 내에 키:값 쌍 뒤에 콤마 자동생성 안함
    
    eslint 설정 추가.
    1. 줄바꿈관련 규칙 x
    2. react import 하지 않을 때 오류x
    3. prop spread 금지규칙 x

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,16 +9,19 @@ module.exports = {
         'plugin:jsx-a11y/recommended',
         'airbnb', // Airbnb 스타일 가이드 사용
         'airbnb/hooks',
-        'airbnb-typescript',
+        'airbnb-typescript'
       ],
       plugins: ['@typescript-eslint'],
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.json'
       },
       rules: {
         // 추가적인 규칙 설정
         '@typescript-eslint/comma-dangle': ['error', 'never'],
-      },
-    },
-  ],
+        'linebreak-style': 0,
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'off'
+      }
+    }
+  ]
 };

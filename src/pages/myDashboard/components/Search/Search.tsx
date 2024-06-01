58c55cb14a67a2e019e0ba8ad3a2925e700390b8
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-06-01 12:27:57 +0900

    [Fix] linter 오류 수정

--- src/pages/myDashboard/components/Search/Search.tsx
+++ src/pages/myDashboard/components/Search/Search.tsx
@@ -6,7 +6,7 @@ function Search() {
   return (
     <div className={styles.container}>
       <img src="/icon/search.svg" alt="돋보기 아이콘" />
-      <input className={styles.searchInput} placeholder="검색"></input>
+      <input className={styles.searchInput} placeholder="검색" />
     </div>
   );
 }

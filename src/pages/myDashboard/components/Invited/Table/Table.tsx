caf6f8a8cb56932532df9502194ac914fc15d6dd
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-06-01 13:29:45 +0900

    [Feat] table 구조 변경, 반응형 디자인 적용
    - table에 반응형 디자인 적용을 위해 구조를 변경했습니다.
    - 초대받은 대시보드 부분에 반응형 디자인을 적용했습니다.

--- src/pages/myDashboard/components/Invited/Table/Table.tsx
+++ src/pages/myDashboard/components/Invited/Table/Table.tsx
@@ -1,12 +1,17 @@
 import TableBody from './TableBody';
 import TableHeader from './TableHeader';
 import styles from './Table.module.scss';
+import TableMobile from './TableMobile';
 
 function Table() {
   return (
     <table className={styles.table}>
       <TableHeader />
-      <TableBody />
+      <tbody>
+        <TableBody />
+        <TableBody />
+        <TableBody />
+      </tbody>
     </table>
   );
 }

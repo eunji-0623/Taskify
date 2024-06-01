10f366ee6728e776431ff3fc5f41aab98b4c483a
Park Sumin <fhsjdvs@gmail.com>
2024-06-01 12:53:16 +0900

    Feat : 랜더링 페이지 구현 및 lint오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import './styles/global.scss';
+import './styles/reset.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+/*
+페이지 라우팅 분리,
+*/
 
 function App() {
-  return <>프로젝트 초기설정</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;

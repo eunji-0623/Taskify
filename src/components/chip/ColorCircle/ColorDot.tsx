677993419e4ea40c85fc5b8d720ef200b364cf46
foxholic9 <roy0915z@naver.com>
2024-06-01 14:10:56 +0900

    Feat: 색상 점 컴포넌트화
    8px짜리 점이 많이 쓰여서 컴포넌트화하였습니다.
    color prop만 색상코드로 넘겨서 사용합니다.
    이후 useMemo를 통해 같은 색 점은 렌더링을 반복하지 않도록 구현해볼 예정입니다.

---
+++ src/components/chip/ColorCircle/ColorDot.tsx
@@ -0,0 +1,22 @@
+import ColorCircle from './ColorCircle';
+
+interface ColorDotProps {
+  color: string;
+}
+
+// 작은 점이 여럿 사용되서 컴포넌트로 분리했습니다.
+// useMemo를 사용하면 같은 색 컴포넌트는 렌더링을 한번만 하게 할 수 있습니다.
+function ColorDot({ color }: ColorDotProps) {
+  const colorCircleProps = {
+    color,
+    diameter: 8
+  };
+
+  return <ColorCircle {...colorCircleProps} />;
+}
+
+export default ColorDot;
+
+// 사용예시
+// <ColorDot color="#00ff37" />
+// <ColorDot color="#ff0000" />

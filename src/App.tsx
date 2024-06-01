42b50f0fe4a8897156752b11e9a0235546d99e52
juan0444 <159929147+juan0444@users.noreply.github.com>
2024-06-01 14:00:47 +0900

    Revert "랜더링 페이지 구현"

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import './styles/global.scss';
-import './styles/reset.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-
-/*
-페이지 라우팅 분리,
-*/
+import React from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <>프로젝트 초기설정</>;
 }
+
 export default App;

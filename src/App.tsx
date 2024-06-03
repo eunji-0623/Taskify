7c1da4126bed94b385a786c829da52fde5e187cf
foxholic9 <roy0915z@naver.com>
2024-06-03 16:41:49 +0900

    Fix: App.tsx 복구
    테스트했던 부분이 커밋과정에서 들어갔습니다.
    원상복구했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,19 @@ import Home from './pages/Home/Home';
 import MyDashboard from './pages/myDashboard';
 import DashboardEdit from './pages/dashboardEdit';
 
-import ColumnCard from './pages/dashboard.{dashboardid}/components/ColumnCard/ColumnCard';
 /*
 페이지 라우팅 분리,
 */
 
 function App() {
-  const cardProps = {
-    imageUrl: '/img/test_img_card_with_people1.png',
-    assignee: 'unknwon',
-    title: '새로운 일정 관리 Taskify',
-    dueDate: new Date('2022-03-13 15:23:37'),
-    tags: ['프로젝트', '상', '1', '22', '333', '454'],
-  };
-
   return (
-    <>
-      <ColumnCard {...cardProps} />
-      <ColumnCard {...cardProps} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/mydashboard" element={<MyDashboard />} />
+        <Route path="/dashboard/1/edit" element={<DashboardEdit />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;

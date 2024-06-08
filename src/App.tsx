bc93dfb8a39b234204555cb49ffbc94b79540a3d
juan0444 <dhwjs5267@gmail.com>
2024-06-08 12:24:30 +0900

    Remove: 테스트 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import Login from './pages/login-signup/login/Login';
 import SignUp from './pages/login-signup/signup/SignUp';
 import MyPage from './pages/mypage/MyPage';
 
-import ModalPageTest from './pages/modalPageTest';
-
 /*
 페이지 라우팅 분리,
 */
@@ -25,9 +23,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/mypage" element={<MyPage />} />
-
-        {/* 삭제하기 */}
-        <Route path="/modal" element={<ModalPageTest />} />
       </Routes>
     </BrowserRouter>
   );

ed30f6ac93f8da52fa33afe8ddd0b9b7d09ea30a
imsoohyeok <skilldoy2@naver.com>
2024-06-15 23:55:59 +0900

    fix: lint

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -15,15 +15,12 @@ const instance = axios.create({
 // 요청 인터셉터를 추가하여 매 요청 시마다 토큰을 헤더에 추가
 instance.interceptors.request.use(
   (config) => {
-    const newConfig = Object.assign({}, config); // 기존 config 객체를 복사
     if (token) {
-      newConfig.headers = Object.assign({}, config.headers, {
-        Authorization: `Bearer ${token}`, // Authorization 필드 추가
-      });
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return newConfig;
+    return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default instance;

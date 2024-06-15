42e8e463f86465ef02bf6249ae7eb81b3ea42687
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-06-15 13:15:06 +0900

    Fix: 초대 보낸 후 새로고침, 초대 모달에 오류 메세지 추가

--- src/pages/modal/InviteModal/InviteModal.tsx
+++ src/pages/modal/InviteModal/InviteModal.tsx
@@ -3,6 +3,7 @@ import ModalContainer from '../ModalContainer/ModalContainer';
 import { apiInviteDashboards } from '../../../api/apiModule';
 import { DeleteBtn } from '../../../components/Btn/Btn';
 import styles from './InviteModal.module.scss';
+import axios, { AxiosError } from 'axios';
 
 /*
   초대하기 모달입니다.
@@ -16,6 +17,7 @@ interface ModalProps {
 
 function InviteModal({ isOpen, setIsOpen, dashboardId }: ModalProps) {
   const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
 
   // 모달 닫기
   const close = useCallback(() => {
@@ -33,11 +35,23 @@ function InviteModal({ isOpen, setIsOpen, dashboardId }: ModalProps) {
         setIsOpen(false);
       }
     } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorResponse = error.response?.status;
+        if (errorResponse === 404) {
+          setError('이메일을 다시 한번 확인해주세요');
+        } else if (errorResponse === 403) {
+          setError('대시보드 초대 권한이 없습니다');
+        }
+      } else {
+        setError('예상치 못한 오류가 발생했습니다');
+      }
       throw new Error('error');
     }
+    window.location.reload();
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setError('');
     setEmail(event.target.value);
   };
 
@@ -59,13 +73,20 @@ function InviteModal({ isOpen, setIsOpen, dashboardId }: ModalProps) {
               onChange={handleChange}
             />
           </div>
+          <p className={error !== '' ? styles.errorMessage : styles.message}>
+            존재하지 않는 유저 입니다.
+          </p>
 
           <div className={styles.buttonBlock}>
             <DeleteBtn BtnText="취소" handleBtn={close} />
             {email.length !== 0 ? (
-              <button className={styles.activeButton} type="submit">초대</button>
+              <button className={styles.activeButton} type="submit">
+                초대
+              </button>
             ) : (
-              <button className={styles.inactiveButton} type="button" disabled>초대</button>
+              <button className={styles.inactiveButton} type="button" disabled>
+                초대
+              </button>
             )}
           </div>
         </form>
